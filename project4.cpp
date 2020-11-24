af54faaa1703f4adec8057f087c0997e78416ba2
Juan <64536309+juanjies@users.noreply.github.com>
2020-11-23 20:03:54 -0500

    Add files via upload

--- project4.cpp
+++ project4.cpp
@@ -49,9 +49,11 @@ int main()
   listOne.getElemAtIndex(4, theVal);
   cout << "the 4th element is: " << theVal << endl; 
   listOne.removeFront(theVal);
-  listOne.removeFront(theVal);
+  listOne.removeLast(theVal);
   listOne.insertValue(4);
   listOne.insertValue(6);
+  listOne.printForward();
+  listOne.clear()
   listOne.printForward();
 //  cout << "List two is: ";
 //  listTwo.printForward();
